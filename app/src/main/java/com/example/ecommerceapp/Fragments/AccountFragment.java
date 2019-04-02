c571aee84f8efc925060f4fb3772f8632ebda5da
talha mughal <talhamughal1997@gmail.com>
2019-04-02 18:17:01 +0500

    Design Layouts

--- app/src/main/java/com/example/ecommerceapp/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/ecommerceapp/Fragments/AccountFragment.java
@@ -13,12 +13,14 @@ import com.example.ecommerceapp.R;
 public class AccountFragment extends Fragment {
 
     View mainView;
+    EditText mEditText_Name, mEditText_Lname, mEditText_DOB, mEditText_Phone, mEditText_Address;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mainView = inflater.inflate(R.layout.fragment_account, container, false);
-
         return mainView;
     }
+
+
 
 }
