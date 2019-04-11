5e7b02e4db76db06eb6a8eb582b7072667530053
Talha Mughal <talhamughal1997>
2019-04-11 19:20:52 +0500

    create firebase user id

--- app/src/main/java/com/example/ecommerceapp/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/ecommerceapp/Fragments/AccountFragment.java
@@ -2,20 +2,26 @@ package com.example.ecommerceapp.Fragments;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.ecommerceapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AccountFragment extends Fragment implements View.OnClickListener {
 
@@ -23,42 +29,54 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
 
     View mainView;
     EditText mEditText_Name, mEditText_Lname, mEditText_DOB, mEditText_Phone, mEditText_Address;
+    Button mBtnSave;
     ImageView mImg_Upload;
     Uri imageUri;
+    ProgressDialog progressDialog;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mDatabaseReference;
 
+    String uid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mainView = inflater.inflate(R.layout.fragment_account, container, false);
         viewInits();
+        viewListeners();
+
 
         return mainView;
     }
 
-    private boolean emptyValidations() {
-        String val1, val2, val3, val4, val5, val6;
+    private void emptyValidations() {
+        String val1, val2, val3, val4, val5;
         val1 = mEditText_Name.getText().toString();
         val2 = mEditText_Lname.getText().toString();
         val3 = mEditText_DOB.getText().toString();
         val4 = mEditText_Phone.getText().toString();
         val5 = mEditText_Address.getText().toString();
 
-
         if (TextUtils.isEmpty(val1.trim()) || TextUtils.isEmpty(val2.trim()) || TextUtils.isEmpty(val3.trim()) || TextUtils.isEmpty(val4.trim()) || TextUtils.isEmpty(val5.trim())) {
             Toast.makeText(getActivity(), "Plz Fill All TextBoxes", Toast.LENGTH_SHORT).show();
-            return false;
+            progressDialog.dismiss();
         }
-        return true;
+
     }
 
     private void viewInits() {
         mEditText_Name = mainView.findViewById(R.id.account_edttxt_name);
-        mEditText_Name = mainView.findViewById(R.id.account_edttxt_lname);
-        mEditText_Name = mainView.findViewById(R.id.account_edttxt_dob);
-        mEditText_Name = mainView.findViewById(R.id.account_edttxt_phone);
-        mEditText_Name = mainView.findViewById(R.id.account_edttxt_address);
+        mEditText_Lname = mainView.findViewById(R.id.account_edttxt_lname);
+        mEditText_DOB = mainView.findViewById(R.id.account_edttxt_dob);
+        mEditText_Phone = mainView.findViewById(R.id.account_edttxt_phone);
+        mEditText_Address = mainView.findViewById(R.id.account_edttxt_address);
         mImg_Upload = mainView.findViewById(R.id.account_img_upload);
-        viewListeners();
+        mBtnSave = mainView.findViewById(R.id.account_btn_Save);
+        progressDialog = new ProgressDialog(getActivity());
+    }
+
+    private void createFirebase() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     private void pickImage() {
@@ -70,6 +88,7 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
 
     private void viewListeners() {
         mImg_Upload.setOnClickListener(this);
+        mBtnSave.setOnClickListener(this);
     }
 
     @Override
@@ -79,7 +98,16 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
                 pickImage();
                 break;
             }
+            case R.id.account_btn_Save: {
+                progressDialog.show();
+                emptyValidations();
+                break;
+            }
         }
+    }
+
+    private void saveInfo() {
+
     }
 
     @Override
--- app/src/main/java/com/example/ecommerceapp/Fragments/SignUpFragment.java
+++ app/src/main/java/com/example/ecommerceapp/Fragments/SignUpFragment.java
@@ -2,20 +2,34 @@ package com.example.ecommerceapp.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.ecommerceapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class SignUpFragment extends Fragment {
 
     View mainView;
     EditText mEditText_Email, mEditText_Pswd, mEditText_CnfrmPswd;
     Button mSignUp;
+    String email, pswd;
+
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,7 +39,12 @@ public class SignUpFragment extends Fragment {
         mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                email = mEditText_Email.getText().toString();
+                pswd = mEditText_Pswd.getText().toString();
+                signUpWithFirebase();
                 changeFragment(new AccountFragment());
+
             }
         });
 
@@ -38,10 +57,35 @@ public class SignUpFragment extends Fragment {
         mEditText_Pswd = mainView.findViewById(R.id.signup_edttxt_pswd);
         mEditText_CnfrmPswd = mainView.findViewById(R.id.signup_edttxt_cnfrm_pswd);
         mSignUp = mainView.findViewById(R.id.btn_SignUp);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
     private void changeFragment(Fragment fragment) {
         getActivity().getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit).replace(R.id.mainActivity_container, fragment).addToBackStack(null).commit();
     }
+
+    private void signUpWithFirebase() {
+        mFirebaseAuth.createUserWithEmailAndPassword(email, pswd)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
 
 }
